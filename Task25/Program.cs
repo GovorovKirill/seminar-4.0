6243cd1338b769f9c51c8e224e942ec49a9b8286
Kirill <kirillgovorov@gmail.com>
2023-07-22 00:07:53 +0300

    Task 25 is done

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("введите первое число");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int y = Convert.ToInt32(Console.ReadLine());
+
+
+int getExtent = GetExtent(x, y);
+Console.Write($"{x}, {y} -> {getExtent}");
+
+int GetExtent(int x1, int y1)
+{
+    int count = 1;
+    int result = 1;
+
+    if (y1 < 0) 
+    {
+        y1 = -y1;
+
+        while (count <= y1) 
+        {
+        count++;
+        result = result * x1;
+        }
+    }
+
+    else 
+    {
+    while (count <= y1) 
+    {
+        count++;
+        result = result * x1;
+    }
+    }
+     return result;
+}
